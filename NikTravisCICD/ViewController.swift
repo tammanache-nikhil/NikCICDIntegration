047f4acdffb9c3dee38af7d9e55cad0740ee470f
tammanachenikhil <tammanache_nikhil@network.lilly.com>
2019-07-29 02:19:21 +0530

    Travis Setup

---
+++ NikTravisCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NikTravisCICD
+//
+//  Created by Tammanache, Nikhil on 28/07/19.
+//  Copyright © 2019 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
